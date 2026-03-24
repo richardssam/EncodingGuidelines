713a086d2facf0ce9748d9674d1c6774f24459a9
Sam.Richards@taurich.org <Sam.Richards@taurich.org>
2026-03-24 20:17:10 +0000

    Need C++ too.
    Signed-off-by: Sam.Richards@taurich.org <Sam.Richards@taurich.org>

--- conan/recipes/libvmaf/all/conanfile.py
+++ conan/recipes/libvmaf/all/conanfile.py
@@ -53,6 +53,6 @@ class LibVmafRecipe(ConanFile):
         self.cpp_info.libs = ["vmaf"]
         self.cpp_info.includedirs = ["include", os.path.join("include", "libvmaf")]
         if self.settings.os in ["Linux", "FreeBSD"]:
-            self.cpp_info.system_libs.append("m")
+            self.cpp_info.system_libs.extend(["m", "stdc++"])
         # This ensures FFmpeg's ./configure finds it easily
         self.cpp_info.set_property("pkg_config_name", "libvmaf")
